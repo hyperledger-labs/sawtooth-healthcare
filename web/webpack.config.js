846849d3922fb78dd887ae6938eda25416889bce
Alexander Zhovnuvaty <a.zhovnuvaty@gmail.com>
2019-08-02 11:15:38 +0300

    Add blockchain network: 1 master and 2 workers
    Signed-off-by: Alexander Zhovnuvaty <a.zhovnuvaty@gmail.com>

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,18 +1,40 @@
-module.exports = {
+//module.exports = {
+//
+//  devServer: {
+//    host: '0.0.0.0',
+//    port: 8080,
+//    proxy: {
+//      '/api': {
+//        target: "http://healthcare-rest-api:8000",
+//        headers: {
+//          "X-real-ip": "0.0.0.0"
+//        },
+//        pathRewrite: {
+//          '^/api' : ''
+//        }
+//      }
+//    }
+//  }
+//};
 
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: "http://healthcare-rest-api:8000",
-        headers: {
-          "X-real-ip": "0.0.0.0"
-        },
-        pathRewrite: {
-          '^/api' : ''
+module.exports = function({ hra = "http://healthcare-rest-api:8000" }) {
+    return {
+
+      devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        proxy: {
+          '/api': {
+            target: hra,
+            headers: {
+              "X-real-ip": "0.0.0.0"
+            },
+            pathRewrite: {
+              '^/api' : ''
+            }
+          }
         }
       }
+
     }
-  }
 };
